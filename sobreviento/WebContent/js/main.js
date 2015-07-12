6c514e5c149b9cefda9689568a466a5df25bf45b
nahuel <nahuel_gta2@hotmail.com>
2015-07-12 19:07:04 -0300

    Video and Music

--- sobreviento/WebContent/js/main.js
+++ sobreviento/WebContent/js/main.js
@@ -79,60 +79,41 @@ $(document).ready(function() {
 	$('.video-content').fitVids();
 });
 
-/* Countdown */
-$(document)
-		.ready(
-				function() {
-					'use strict';
-					var labels = [ 'weeks', 'days', 'hours', 'minutes',
-							'seconds' ], nextYear = (new Date().getFullYear() + 1)
-							+ '/01/01', currDate = '00:00:00:00:00', nextDate = '00:00:00:00:00', parser = /([0-9]{2})/gi, $example = $('#countdown');
-					// Parse countdown string to an object
-					function strfobj(str) {
-						var parsed = str.match(parser), obj = {};
-						labels.forEach(function(label, i) {
-							obj[label] = parsed[i]
-						});
-						return obj;
-					}
-					// Return the time components that diffs
-					function diff(obj1, obj2) {
-						var diff = [];
-						labels.forEach(function(key) {
-							if (obj1[key] !== obj2[key]) {
-								diff.push(key);
-							}
-						});
-						return diff;
-					}
-					// Starts the countdown
-					$example.countdown(nextYear, function(event) {
-						var newDate = event.strftime('%w:%d:%H:%M:%S'), data;
-						if (newDate !== nextDate) {
-							currDate = nextDate;
-							nextDate = newDate;
-							// Setup the data
-							data = {
-								'curr' : strfobj(currDate),
-								'next' : strfobj(nextDate)
-							};
-							// Apply the new values to each node that changed
-							diff(data.curr, data.next).forEach(
-									function(label) {
-										var selector = '.%s'.replace(/%s/,
-												label), $node = $example
-												.find(selector);
-										// Update the node
-										$node.removeClass('flip');
-										$node.find('.curr').text(
-												data.curr[label]);
-										$node.find('.next').text(
-												data.next[label]);
-										// Wait for a repaint to then flip
-										_.delay(function($node) {
-											$node.addClass('flip');
-										}, 50, $node);
-									});
-						}
-					});
-				});
+/* BXslider */
+$(document).ready(function() {
+	'use strict';
+	$('.bxslider').bxSlider({
+		video : true,
+		useCSS : false
+	});
+});
+
+/* LiteAccordion */
+$(document).ready(function() {
+	'use strict';
+	$('.liteAccordion').liteAccordion({
+		containerWidth : 960,
+		containerHeight : 490,
+		headerWidth : 48,
+
+		theme : 'dark',
+		rounded : true,
+		enumerateSlides : true,
+		firstSlide : 2,
+		linkable : true,
+		easing : 'easeInOutQuart',
+
+		activateOn : 'click',
+		firstSlide : 1,
+		slideSpeed : 500,
+		onTriggerSlide : function() {
+		},
+		onSlideAnimComplete : function() {
+		},
+
+		autoPlay : false,
+		pauseOnHover : false,
+		cycleSpeed : 6000,
+		easing : 'swing'
+	});
+});
